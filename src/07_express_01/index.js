5cbeb6d6338858993dbc0ab8d73c1b3adf557e96
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-10-22 19:26:45 -0500

    Archivos estaticos, carpeta public

--- src/07_express_01/index.js
+++ src/07_express_01/index.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const app = express();
+//enviando archivos estáticos
+app.use(express.static("public"));
 
+//Enviando la respuesta con la función send
 app.get("/", (req, res) => {
+    res.send("Solo tengo sueño en cantidades industriales");
+});
+
+
+//Enviando archivos
+app.get("/keywords", (req, res) => {
+    const keywords = {
+        algo: ["nodejs", "express"],
+        algomas: 2
+    };
+    res.send(keywords);
+});
+
+//Enviando archivos
+app.get("/archivo", (req, res) => {
+    res.sendFile(__dirname + "/public/archivo3.txt");
+});
+
+//Ejercicio básico
+app.get("/basico", (req, res) => {
     res.writeHead(200);
     res.write("Solo tengo sueño en cantidades industriales");
     res.end();
